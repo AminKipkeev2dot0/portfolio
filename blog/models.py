028b23403ba1f28dc3ce3c792307a56d28bb461b
AminKipkeev2dot0 <aminkipkeev12@gmail.com>
2021-07-15 22:43:33 +0300

    Models of blog are in html succesfully

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class blog_post(models.Model):
+    title = models.CharField(max_length=30)
+    desc = models.TextField(max_length=300)
+    dateofpost = models.DateField(auto_now=False, auto_now_add=False)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import blog_post
 
 # Create your views here.
+def blogpage(request):
+    all_posts = blog_post.objects.all()
+    return render(request, 'blog/blog.html', {'all_posts':all_posts})
