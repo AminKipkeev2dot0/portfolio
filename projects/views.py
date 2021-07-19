426146c4d65af80636cdafdc7a3c4aa6beb54fc4
AminKipkeev2dot0 <aminkipkeev12@gmail.com>
2021-07-20 02:37:10 +0300

    all is ready except little bit

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import project
+from blog.models import blog_post
 
 # Create your views here.
 def home(request):
+    all_posts = blog_post.objects.all()[:3]
     all_projects = project.objects.all()
-    return render(request, 'projects/home.html', {'all_projects': all_projects})
+    return render(request, 'projects/home.html', {'all_projects': all_projects, 'all_posts':all_posts})
