943be2c2fd18e873ae25a6b4f3094ad538af2d9a
AminKipkeev2dot0 <aminkipkeev12@gmail.com>
2021-07-25 12:37:20 +0300

    fix

--- projects/views.py
+++ projects/views.py
@@ -4,6 +4,6 @@ from blog.models import blog_post
 
 # Create your views here.
 def home(request):
-    all_posts = blog_post.objects.all()[-3:]
+    all_posts = blog_post.objects.all()[-3:-1]
     all_projects = project.objects.all()
     return render(request, 'projects/home.html', {'all_projects': all_projects, 'all_posts':all_posts})
